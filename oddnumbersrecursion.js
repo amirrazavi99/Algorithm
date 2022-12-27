2fe4edad811300d14a77c62cf89e7534e4c30a1b
amirrazavi <amirrazavi99manesh@gmail.com>
2022-12-28 00:18:29 +0330

    oddnumberrecursion

---
+++ oddnumbersrecursion.js
@@ -0,0 +1,15 @@
+function oddArray(arr) {
+    let result = [];
+    function helperRecursiveFn(arr) {
+        if(arr.length === 0) {
+            return; // 1
+        } else if(arr[0] % 2 !== 0) {
+            result.push(arr[0]); // 2
+        }
+        helperRecursiveFn(arr.slice(1)); // 3
+    }
+    helperRecursiveFn(arr);
+    return result;
+}
+
+console.log(oddArray([13, 22, 5, 14, 15, 20, 9, 43, 19]));  //[ 13, 5, 15, 9, 43, 19 ]
